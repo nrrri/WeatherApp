197be356ade341a28a34f2ba035a2486cdf46eb9
Ferin.Chow <nrrri.cho@gmail.com>
2024-07-27 14:20:40 -0400

    fixed api function

---
+++ WeatherApp/Greeting.swift
@@ -0,0 +1,18 @@
+//
+//  Greeting.swift
+//  WeatherApp
+//
+//  Created by Narissorn Chowarun on 2024-07-27.
+//
+
+import SwiftUI
+
+struct Greeting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Greeting()
+}
